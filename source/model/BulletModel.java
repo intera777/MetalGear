655688b8ae2bce5904eef2f75aba8a90d8c214f8
intera777 <interactive.imgur@gmail.com>
2025-12-12 22:18:15 +0900

    BulletModelを少し変更

--- source/model/BulletModel.java
+++ source/model/BulletModel.java
@@ -3,5 +3,7 @@ package model;
 import java.util.Observable;
 
 public class BulletModel.java extends Observable{
-      
+      private int x=-100;
+      private int y=-100;
+      private boolean exist=false; //フィールド上に存在していればtrue.
 }
