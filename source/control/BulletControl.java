c607882b53b6d81d6ed71b158ce2d148dc5959f1
yasutakekaito <y2411753@gl.cc.uec.ac.jp>
2025-12-15 14:19:58 +0900

    BulletControllの変更
    銃を単発で撃てるようにするために、Jが押され続ける時ではなく、新しくJキーが押された時のみ銃が発射されるようにブーリアンを用いてコードを書いた。また、銃を発射する際に弾の配列を作り、撃たれた弾がマップ外に飛ばされた時に弾がその配列に戻るようにして銃のControllとした。

--- source/control/BulletControl.java
+++ source/control/BulletControl.java
@@ -1,27 +1,49 @@
 package control;
 
+import model.BulletModel;
 import model.PlayerModel;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class BulletControl implements KeyListener{
-      private BulletModel model;
+public class BulletControl implements KeyListener {
+    private BulletModel[] bulletModels;
+    private PlayerModel playerModel;
+    private boolean isJPressed = false;
 
-      public BulletControl(BulletModel model){
-            this.model=model;
-      }
-
-      @Override
-      public void KeyTyped(KeyEvent e){}
-
-      @Override
-    public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
-        if(code)
-        
+    public BulletControl(BulletModel[] bulletModels, PlayerModel playerModel) {
+        this.bulletModels = bulletModels;
+        this.playerModel = playerModel;
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_J) {
+            if (!isJPressed) {
+                fire(); 
+                isJPressed = true;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_J) {
+            isJPressed = false;
+        }
+    }
+
+    private void fire() {
+        for (BulletModel bullet : bulletModels) {
+            // 「今、この弾は画面上に存在していない（=使っていない）か？」を確認
+            if (!bullet.bulletExist()) {
+                // プレイヤーの場所（X, Y）に弾をセットして「発射状態」にする
+                bullet.shoot(playerModel.getPlayerX(), playerModel.getPlayerY());
+                break;
+            }
+        }
+    }
 }
